c6ed9ff9ce356c79aa71b6a0dfa50a50b7a6203d
BaoWoW <baonguyen9392@gmail.com>
2022-07-12 15:16:20 -0700

    Addition of Five Rounds & Scorekeeping
    Added for loop for five rounds.
    Included scorekeeping, but does not notify final winner/loser at the end.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,51 @@
+let playerScore=0;
+let computerScore=0;
+
+
+for (i=0;i<5;i++) {
 const choices = ['rock','paper','scissors'];
 const random = Math.floor(Math.random() * choices.length);
   
     function computerPlay(){
     return (choices[random]);
-};
+    };
 
 let playerSelection= prompt('Pick');
 let player = playerSelection.toLowerCase();
 
-function game() {
+
+function playRound() {
   if (playerSelection.toLowerCase() == computerPlay()){
     return 'Tie!'
   }
   else  if ((player == 'rock' && computerPlay() =='scissors') ||
            (player == 'scissors' && computerPlay() == 'paper') ||
            (player == 'paper' && computerPlay() == 'rock')) {
-    return "You won!"
+    playerScore = playerScore+1;
+    return "You won!";
   }
   else if ((player == 'rock' && computerPlay() == 'paper') ||
            (player == 'paper' && computerPlay() == 'scissors') ||
            (player == 'scissors' && computerPlay() == 'rock')){
+    computerScore = computerScore+1;
     return 'You lost!';
   }
   else {
     return 'Not a choice!'
   };
 }
-console.log(computerPlay());
 
-console.log(game());
+console.log(playRound());
+console.log(playerScore,computerScore);
+}
 
+
+//stopped here
+  function game() {
+  if (playerScore>computerScore) {
+    return 'You win!'
+  }
+  else if (playerScore<computerScore){
+    return 'You loss!'
+  }
+}
